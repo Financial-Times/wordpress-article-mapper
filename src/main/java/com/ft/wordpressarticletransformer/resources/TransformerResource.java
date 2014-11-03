2f16bf981087088f75949f9b2da7a71c9310663e
Sarah Wells <sarah.wells@ft.com>
2014-11-03 17:00:48 +0000

    Instantiate the ObjectMapper correctly, and use the right DateTime format

--- src/main/java/com/ft/wordpressarticletransformer/resources/TransformerResource.java
+++ src/main/java/com/ft/wordpressarticletransformer/resources/TransformerResource.java
@@ -3,10 +3,9 @@ package com.ft.wordpressarticletransformer.resources;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Arrays;
-import java.util.Date;
-import java.util.Map;
 import java.util.TreeSet;
 import java.util.UUID;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -16,12 +15,14 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.codahale.metrics.annotation.Timed;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.ft.api.jaxrs.errors.ClientError;
 import com.ft.api.jaxrs.errors.ServerError;
 import com.ft.api.util.transactionid.TransactionIdUtils;
 import com.ft.bodyprocessing.BodyProcessingException;
@@ -29,11 +30,8 @@ import com.ft.content.model.Brand;
 import com.ft.content.model.Content;
 import com.ft.wordpressarticletransformer.response.WordPressResponse;
 import com.ft.wordpressarticletransformer.transformer.BodyProcessingFieldTransformer;
-import com.google.common.collect.Maps;
 import com.sun.jersey.api.NotFoundException;
 import com.sun.jersey.api.client.ClientResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Path("/content")
 public class TransformerResource {
@@ -42,7 +40,7 @@ public class TransformerResource {
 
     private static final String CHARSET_UTF_8 = ";charset=utf-8";
 
-	public static final String ORIGINATING_SYSTEM_FT_CLAMO = "http://www.ft.com/ontology/origin/TODO_USE_CORRECT_VALUE";
+	public static final String ORIGINATING_SYSTEM_FT_WORDPRESS = "http://www.ft.com/ontology/origin/TODO_USE_CORRECT_VALUE";
 
     private final BodyProcessingFieldTransformer bodyProcessingFieldTransformer;
 	private final Brand fastFtBrand;//TODO replace with brand lookup
@@ -60,7 +58,7 @@ public class TransformerResource {
 	@Timed
 	@Path("/{uuid}")
 	@Produces(MediaType.APPLICATION_JSON + CHARSET_UTF_8)
-	public final Content getByPostId(@PathParam("uuid") UUID uuid, @QueryParam("url") URI requestUri, @Context HttpHeaders httpHeaders) {
+	public final Content getByPostId(@PathParam("uuid") String uuid, @QueryParam("url") URI requestUri, @Context HttpHeaders httpHeaders) {
 
 	    String transactionId = TransactionIdUtils.getTransactionIdOrDie(httpHeaders, uuid, "Publish request");
 	    
@@ -70,20 +68,22 @@ public class TransformerResource {
 			throw new NotFoundException();
 		}
 
-		String title = "title";
-		String body = transformBody("<p>Hard coded body</p>");
-		Date datePublished = new Date();
-
+		String body = wrapBody(wordPressResponse.getPost().getContent());
+		
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss"); //2014-10-21 05:45:30
+		DateTime datePublished = formatter.parseDateTime(wordPressResponse.getPost().getDate());
+		
 		LOGGER.info("Returning content for uuid [{}].", uuid);
 		
 		Brand brand = getBrand(requestUri);        
 
-		return Content.builder().withTitle(title)
-				.withPublishedDate(datePublished)
+		return Content.builder().withTitle(wordPressResponse.getPost().getTitle())
+				.withPublishedDate(datePublished.toDate())
 				.withXmlBody(tidiedUpBody(body, transactionId))
-				.withContentOrigin(ORIGINATING_SYSTEM_FT_CLAMO, uuid.toString())
+				.withByline(wordPressResponse.getPost().getAuthor().getName())
+				.withContentOrigin(ORIGINATING_SYSTEM_FT_WORDPRESS, uuid.toString())
 				.withBrands(new TreeSet<>(Arrays.asList(brand)))
-				.withUuid(uuid).build();
+				.withUuid(UUID.fromString(uuid)).build();
 
 	}
 
@@ -100,7 +100,7 @@ public class TransformerResource {
         }
 	}
 
-	private String transformBody(String originalBody) {
+	private String wrapBody(String originalBody) {
 		return "<body>" + originalBody + "</body>";
 	}
 
@@ -124,21 +124,16 @@ public class TransformerResource {
         
         String json = rawOutput.substring(rawOutput.indexOf("{"));
         
-        final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+        final ObjectMapper objectMapper = new ObjectMapper();
         WordPressResponse wordPressResponse = null;
         
         try {
             wordPressResponse = objectMapper.readValue(json, WordPressResponse.class);
-        } catch (JsonParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            LOGGER.error("Failed to parse response from WordPress", e);
+            throw ServerError.status(500).error("Failed to parse response from WordPress").exception(e);
         }
+        
 
         return wordPressResponse;
     }
