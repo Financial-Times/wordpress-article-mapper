eaf38dfff72ba542337634a0212ad8e0b465a7a1
Mike Huniewicz <michal.huniewicz@ft.com>
2014-11-28 12:06:50 +0000

    Use correct Wordpress API key.

--- src/test/java/com/ft/wordpressarticletransformer/resources/WP.java
+++ src/test/java/com/ft/wordpressarticletransformer/resources/WP.java
@@ -7,5 +7,5 @@ package com.ft.wordpressarticletransformer.resources;
  */
 public class WP {
 
-	public static final String EXAMPLE_API_KEY = "ABC123DEF456ABC123DEF456ABC123DEF456";
+	public static final String EXAMPLE_API_KEY = "75ec83ae259076b1224ac5ce28f5e5d3d02c1cacc04e4c537ed2578ff8441d09";
 }
