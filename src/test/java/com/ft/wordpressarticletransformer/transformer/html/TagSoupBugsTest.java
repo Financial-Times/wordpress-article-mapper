b01e88d6ce3f75d28c8b568685255282a37d3a58
Scott.Newton <scott.newton@ft.com>
2015-01-19 15:30:20 +0000

    Fixed failing tests

--- src/test/java/com/ft/wordpressarticletransformer/transformer/html/TagSoupBugsTest.java
+++ src/test/java/com/ft/wordpressarticletransformer/transformer/html/TagSoupBugsTest.java
@@ -15,7 +15,7 @@ public class TagSoupBugsTest {
 
     @Test @Ignore
     public void wanderingATagTest() {
-        String body = "<div data-asset-type=\"embed\"><blockquote class=\"twitter-tweet\" lang=\"en\"><p>Learning from Comcast/TWC? AT&amp;T b DirecTV deal includes collar protecting <a href=\"https://twitter.com/search?q=%24DTV&amp;src=ctag\">$DTV</a> shareholders from decline in <a href=\"https://twitter.com/search?q=%24T&amp;src=ctag\">$T<\\/a> stock. (Caps upside, too).</p>&mdash; Liz Hoffman (@lizrhoffman) <a href=\"https://twitter.com/lizrhoffman/statuses/468146880682016769\">May 18, 2014</a></blockquote><script src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script></div>";
+        String body = "<div data-asset-type=\"embed\"><blockquote class=\"twitter-tweet\" lang=\"en\"><p>Learning from Comcast/TWC? AT&amp;T b DirecTV deal includes collar protecting <a href=\"https://twitter.com/search?q=%24DTV&amp;src=ctag\">$DTV</a> shareholders from decline in <a href=\"https://twitter.com/search?q=%24T&amp;src=ctag\">$T</a> stock. (Caps upside, too).</p>&mdash; Liz Hoffman (@lizrhoffman) <a href=\"https://twitter.com/lizrhoffman/statuses/468146880682016769\">May 18, 2014</a></blockquote><script src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script></div>";
         tagSoupHtmlBodyProcessor = new TagSoupHtmlBodyProcessor();
         String result = tagSoupHtmlBodyProcessor.process(body, new BodyProcessingContext() {});
         assertThat("does not contain", result, containsString("\">$T<\\/a>"));
