764949de7fe11b34ca9b35b1e20a7e2152bfc4ed
Keith Hatton <keith.hatton@ft.com>
2016-04-08 10:53:17 +0100

    Reinstate featured image as mainImage property only.
    It has been decided that WP content should not by default have the
    featured image inserted at the start of the body.

--- src/main/java/com/ft/wordpressarticletransformer/transformer/WordPressBlogPostContentTransformer.java
+++ src/main/java/com/ft/wordpressarticletransformer/transformer/WordPressBlogPostContentTransformer.java
@@ -7,9 +7,6 @@ import java.util.Objects;
 import java.util.SortedSet;
 import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.ft.wordpressarticletransformer.model.Brand;
 import com.ft.wordpressarticletransformer.model.Identifier;
 import com.ft.wordpressarticletransformer.model.WordPressBlogPostContent;
@@ -24,12 +21,8 @@ import com.google.common.collect.ImmutableSortedSet;
 public class WordPressBlogPostContentTransformer
         extends WordPressContentTransformer<WordPressBlogPostContent> {
     
-  private static final Logger LOG = LoggerFactory.getLogger(WordPressBlogPostContentTransformer.class);
-  
   private static final String START_BODY = "<body>";
   private static final String END_BODY = "</body>";
-  private static final String MAIN_IMAGE_XML =
-      "<content data-embedded=\"true\" id=\"%s\" type=\"http://www.ft.com/ontology/content/ImageSet\"></content>";
   
     private final BodyProcessingFieldTransformer bodyProcessingFieldTransformer;
     
@@ -57,29 +50,23 @@ public class WordPressBlogPostContentTransformer
                 .withBrands(brands)
                 .withIdentifiers(ImmutableSortedSet.of(new Identifier(originatingSystemId, post.getUrl())))
                 .withComments(createComments(post.getCommentStatus()))
-//                .withMainImage(Objects.toString(featuredImageUuid, null))
+                .withMainImage(Objects.toString(featuredImageUuid, null))
                 .withPublishReference(transactionId)
                 .withLastModified(lastModified);
         
-        String transformedBody = transformHtml(body, featuredImageUuid, transactionId);
+        String transformedBody = transformHtml(body, transactionId);
         if (Strings.isNullOrEmpty(unwrapBody(transformedBody))) {
           throw new UntransformablePostException(uuid.toString(), "Not a valid WordPress article for publication - body of transformed post is empty");
         }
         
         builder = builder.withBody(transformedBody)
-                         .withOpening(transformHtml(wrapBody(post.getExcerpt()), featuredImageUuid, transactionId));
+                         .withOpening(transformHtml(wrapBody(post.getExcerpt()), transactionId));
         
         return builder.build();
     }
     
-    private String transformHtml(String html, UUID featuredImageUuid, String transactionId) {
+    private String transformHtml(String html, String transactionId) {
         String transformed = bodyProcessingFieldTransformer.transform(html, transactionId);
-        /*if (featuredImageUuid != null) {
-          int i = transformed.indexOf(START_BODY) + START_BODY.length();
-          transformed = transformed.substring(0,  i)
-              + String.format(MAIN_IMAGE_XML, featuredImageUuid)
-              + transformed.substring(i);
-        }*/
         return transformed;
     }
     
