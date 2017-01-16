371374988f7f973ddb6dbb95fb355e83badbda38
copaciu <daniel.copaciu@ft.com>
2017-01-16 15:27:45 +0200

    improve error handling for mapping endpoint

--- src/main/java/com/ft/wordpressarticlemapper/resources/WordPressArticleMapperResource.java
+++ src/main/java/com/ft/wordpressarticlemapper/resources/WordPressArticleMapperResource.java
@@ -1,8 +1,10 @@
 package com.ft.wordpressarticlemapper.resources;
 
 import com.codahale.metrics.annotation.Timed;
+import com.ft.api.jaxrs.errors.ClientError;
 import com.ft.api.jaxrs.errors.ServerError;
 import com.ft.api.util.transactionid.TransactionIdUtils;
+import com.ft.wordpressarticlemapper.exception.WordPressContentException;
 import com.ft.wordpressarticlemapper.model.WordPressContent;
 import com.ft.wordpressarticlemapper.response.NativeWordPressContent;
 import com.ft.wordpressarticlemapper.response.Post;
@@ -11,6 +13,8 @@ import com.ft.wordpressarticlemapper.transformer.BodyProcessingFieldTransformer;
 import com.ft.wordpressarticlemapper.transformer.WordPressBlogPostContentMapper;
 import com.ft.wordpressarticlemapper.transformer.WordPressContentMapper;
 import com.ft.wordpressarticlemapper.transformer.WordPressLiveBlogContentMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -22,8 +26,10 @@ import javax.ws.rs.core.MediaType;
 import static org.apache.http.HttpStatus.SC_UNPROCESSABLE_ENTITY;
 
 
-@Path("/content")
+@Path("/")
 public class WordPressArticleMapperResource {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WordPressArticleMapperResource.class);
 
     private static final String CHARSET_UTF_8 = ";charset=utf-8";
 
@@ -44,12 +50,34 @@ public class WordPressArticleMapperResource {
     public final WordPressContent map(NativeWordPressContent nativeWordPressContent, @Context HttpHeaders httpHeaders) {
         Post postDetails = nativeWordPressContent.getPost();
         String transactionId = TransactionIdUtils.getTransactionIdOrDie(httpHeaders, postDetails.getUuid(), "Publish request");
-        return getWordPressContent(nativeWordPressContent, transactionId);
+        try {
+            return getWordPressContent(nativeWordPressContent, transactionId);
+        } catch (IllegalArgumentException | WordPressContentException e) {
+            throw new ClientError.ClientErrorBuilder(SC_UNPROCESSABLE_ENTITY).error("Wordpress content is not valid").exception(e);
+        }
     }
 
     private WordPressContent getWordPressContent(NativeWordPressContent nativeWordPressContent, String transactionId) {
+        validateContent(nativeWordPressContent, transactionId);
         Post postDetails = nativeWordPressContent.getPost();
         return transformerFor(postDetails).mapWordPressArticle(transactionId, postDetails, nativeWordPressContent.getLastModified());
+    }
+
+    private void validateContent(NativeWordPressContent nativeWordPressContent, String transactionId) {
+        Post post = nativeWordPressContent.getPost();
+        if (post == null) {
+            LOGGER.error("Post is missing for request with tid=[{}]", transactionId);
+            throw new IllegalArgumentException("No post supplied");
+        }
+
+        if (nativeWordPressContent.getApiUrl() == null) {
+            throw new IllegalArgumentException("No apiUrl supplied");
+        }
+
+        String postUrl = post.getUrl();
+        if (postUrl == null) {
+            throw new IllegalArgumentException("No post Url supplied");
+        }
     }
 
     private WordPressContentMapper<?> transformerFor(Post post) {
